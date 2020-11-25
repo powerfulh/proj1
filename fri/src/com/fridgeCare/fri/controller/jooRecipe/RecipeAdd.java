c33ed7378f36360ffc9d32a2b61db9766d731e3d
class02 <class02@host.docker.internal>
2020-11-25 12:04:30 +0900

    1st commit

---
+++ fri/src/com/fridgeCare/fri/controller/jooRecipe/RecipeAdd.java
@@ -0,0 +1,16 @@
+package com.fridgeCare.fri.controller.jooRecipe;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fridgeCare.fri.controller.Caller;
+
+public class RecipeAdd implements Caller {
+
+	@Override
+	public String exec(HttpServletRequest req, HttpServletResponse resp) {
+		String view = "joo/scale";
+		return view;
+	}
+
+}
