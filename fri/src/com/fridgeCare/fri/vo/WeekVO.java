d934e9a52760d196a5fc2cfc4d83cf2f9c4e3553
class02 <class02@192.168.0.34>
2020-11-25 20:24:49 +0900

    hh final commit

---
+++ fri/src/com/fridgeCare/fri/vo/WeekVO.java
@@ -0,0 +1,30 @@
+package com.fridgeCare.fri.vo;
+
+public class WeekVO {
+	int bno;
+	String tname , title , id;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
